7a645590e6af401952094ae1b04fd7c2130b9de5
chenan02 <chenan02@dobest.com>
2026-04-11 15:53:44 +0800

    创建定时任务功能优化

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,18 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
+import updateLocale from 'dayjs/plugin/updateLocale'
 import 'ant-design-vue/dist/reset.css'
 import router from './router'
 import App from './App.vue'
 import './style.css'
+
+dayjs.extend(updateLocale)
+dayjs.locale('zh-cn')
+dayjs.updateLocale('zh-cn', {
+  weekStart: 1,
+})
 
 const app = createApp(App)
 
